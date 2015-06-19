e375321c48e9c9e677a75119de4d620e135ffd6e
Yuri Fontella <yurifc4@gmail.com>
2015-06-19 16:58:54 -0300

    database fail

--- app/view/aluno/matricula.php
+++ app/view/aluno/matricula.php
@@ -1,11 +1,11 @@
 <form method="post" action="<?php echo BASE_URL; ?>index.php?c=AlunoController&m=matricula">
-<label>Nome</label>
-<input type="text" name="nome">
-<label>CPF</label>
-<input type="text" name="cpf">
-<label>FONE</label>
-<input type="text" name="fone">
-<label>DATA NASCIMENTO</label>
-<input type="text" name="data_nascimento">
-<button type="submit">Enviar</button>
+	<label>Nome</label>
+	<input type="text" name="nome">
+	<label>CPF</label>
+	<input type="text" name="cpf">
+	<label>FONE</label>
+	<input type="text" name="fone">
+	<label>DATA NASCIMENTO</label>
+	<input type="text" name="data_nascimento">
+	<button type="submit">Enviar</button>
 </form>
