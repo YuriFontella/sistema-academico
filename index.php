583038eef26b1e894b51793eca6c070b886946ca
Yuri Fontella <yurifc4@gmail.com>
2015-06-22 10:16:13 -0300

    commit

--- index.php
+++ index.php
@@ -5,9 +5,17 @@
 require_once 'config' . DIRECTORY_SEPARATOR . 'config.php';
 
 $url = substr($_SERVER['REQUEST_URI'], 1); // Url de preodução /site/controller/method/parameter
+list($c, $m, $p) = explode('/', $url);
+$c = ucfirst($c) . 'Controller';
+if($m == null)
+{
+	$c = 'HomeController';
+	$m = 'index';
+}
 
+/*
 
-if (strpos($url, 'ifc/_sistema-academico') != 0) { 
+if (strpos($url, 'ifc/_sistema-academico') != 0) {
     $value = explode('/', $url);
     $c = (isset($value[0]) ? $value[0] : ''); // Controller
     $m = (isset($value[1]) ? $value[1] : ''); // Metodo
@@ -23,10 +31,11 @@ if (strpos($url, 'ifc/_sistema-academico') != 0) {
     if (count($value) > 4) {
         $p = array_slice($value, 4); // Parametro
     } else {
-        $p = array(); 
+        $p = array();
     }
 }
 
+*/
 
 $c = ucfirst($c) . 'Controller';
 if ($m == null) {
@@ -48,4 +57,3 @@ if (!is_array($p)) {
 $content = call_user_func_array(array($controller, $m), $p);
 
 include_once APP . DS . 'web' . DS . 'index.php';
-
